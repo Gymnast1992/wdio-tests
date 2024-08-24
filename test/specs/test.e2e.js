f84d46ccd73d712718443245fc8d9b82ec249fbd
Maksym Semiankiv <1992maksym1992@gmail.com>
2024-08-24 13:43:46 +0200

    Homework 1

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -10,7 +10,7 @@ describe('Webdriverio main page', () => {
         await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO')
     });
 
-    it("should show addValue command", async () => {
+    xit("should show addValue command", async () => {
         await browser.url(`https://the-internet.herokuapp.com/login`);
 
         let input = await $("#username")
@@ -22,4 +22,83 @@ describe('Webdriverio main page', () => {
 
         await expect(input).toHaveValue("hello123")
     });
+
+    xit("should show setValue command", async () => {
+        await browser.url(`https://the-internet.herokuapp.com/login`);
+
+        let input = await $("#username")
+        await input.setValue("world")
+        await browser.pause(2000)
+        await input.setValue("hello")
+        await browser.pause(2000)
+
+        console.log(await input.getValue())
+        await expect(input).toHaveValue("hello")
+    });
+
+    xit("should show click command", async () => {
+        await browser.url(`https://the-internet.herokuapp.com/login`);
+
+        let loginButton = await $('.radius')
+        await browser.pause(2000)
+        await loginButton.click()
+        await browser.pause(4000)
+
+        let inputUsername = await $("#username")
+        await inputUsername.addValue("tomsmith")
+        await browser.pause(2000)
+
+        let inputPassword = await $("password")
+        await inputPassword.addValue("SuperSecretPassword")
+        await browser.pause(2000)
+
+        await loginButton.click()
+        await browser.pause(4000)
+    });
+
+    xit("should show getAttribute command", async () => {
+        await browser.url(`https://dou.ua/search`);
+
+        let inputSearch = await $('#gsc-i-id1')
+        let attr = await inputSearch.getAttribute("aria-label")
+        console.log("Placeholder attribute is: " + attr)// output: search
+
+        await inputSearch.setValue("Cat")
+        attr = await inputSearch.getValue()
+        await browser.pause(2000)
+        console.log("Value attribute is: " + attr)// output cat
+    });
+
+    xit("should show getLocation command", async () => {
+        await browser.url(`https://dou.ua`);
+
+        let inputSearch = await $('#txtGlobalSearch')
+        let location = await inputSearch.getLocation()
+        console.log("Location is: " + location)// output: x, y
+
+        let xLocation = await inputSearch.getLocation("x")
+        console.log("Location by x is: " + xLocation)// output: x
+    });
+
+    xit("should show getText command", async () => {
+        await browser.url(`https://webdriver.io`);
+
+        let subtitle = await $(`.hero__subtitle`)
+        console.log("Subtitle text is: " + await subtitle.getText())// output: Next-gen browser..
+    })
+    //Homework
+    it("should show getText command", async () => {
+        await browser.url(`https://webdriver.io`);
+
+        let apiButton = await $('//a[text()="API"]');
+        await apiButton.click();
+
+        let text = await $("h1");
+        console.log("The text is: " + await text.getText());
+        await expect(text).toHaveValue("Introduction");
+
+
+        //let subtitle = await $(`.hero__subtitle`)
+        //console.log("Subtitle text is: " + await subtitle.getText())// output: Next-gen browser..
+    })
 });
