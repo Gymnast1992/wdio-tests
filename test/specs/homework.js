6abd5f85a3308bb919c6dbdd2d107dbb691badd2
Maksym Semiankiv <1992maksym1992@gmail.com>
2024-08-26 15:12:09 +0200

    semiankiv.art website tests

--- test/specs/homework.js
+++ test/specs/homework.js
@@ -1,62 +1,62 @@
 
-import { expect } from '@wdio/globals'
+// import { expect } from '@wdio/globals'
 
 
-describe("WebdriverIO homework", () => {
-    xit("should be able to click on the API button", async () => {
-        await browser.url(`https://webdriver.io/`)
-        await browser.pause(2000)
+// describe("WebdriverIO homework", () => {
+//     xit("should be able to click on the API button", async () => {
+//         await browser.url(`https://webdriver.io/`)
+//         await browser.pause(2000)
 
 
-        let buttonAPI = await $('nav [href="/docs/api"]')
-        await buttonAPI.click()
-        await browser.pause(2000)
+//         let buttonAPI = await $('nav [href="/docs/api"]')
+//         await buttonAPI.click()
+//         await browser.pause(2000)
 
-    })
+//     })
 
-    xit("should scroll to the footer blog button", async () => {
-        await browser.url(`https://webdriver.io`);
+//     xit("should scroll to the footer blog button", async () => {
+//         await browser.url(`https://webdriver.io`);
         
-        const buttonBlog = await $(`.footer__link-item[href="/blog"]`)
-        await browser.pause(2000)
-        await buttonBlog.scrollIntoView()
-        await browser.pause(2000)
+//         const buttonBlog = await $(`.footer__link-item[href="/blog"]`)
+//         await browser.pause(2000)
+//         await buttonBlog.scrollIntoView()
+//         await browser.pause(2000)
         
-    })
+//     })
 
-    xit("the link `becoming a sponsor` should be displayed", async () => {
-        await browser.url(`https://webdriver.io`);
+//     xit("the link `becoming a sponsor` should be displayed", async () => {
+//         await browser.url(`https://webdriver.io`);
 
-        const theLink = await $('p [href="/docs/sponsor"]')
-        let isDisplayed = await theLink.isDisplayed()
-        console.log("The link `becoming a sponsor` is displayed: " + isDisplayed)
+//         const theLink = await $('p [href="/docs/sponsor"]')
+//         let isDisplayed = await theLink.isDisplayed()
+//         console.log("The link `becoming a sponsor` is displayed: " + isDisplayed)
 
-    })
+//     })
 
-    xit("the link should be clickable ", async () => {
-        await browser.url(`https://webdriver.io`);
+//     xit("the link should be clickable ", async () => {
+//         await browser.url(`https://webdriver.io`);
 
-        const theLink = await $('p [href="/docs/sponsor"]')
-        let isClickable = await theLink.isClickable()
-        console.log("The link `becoming a sponsor` is clickable: " + isClickable)
-        await theLink.click()
-        await browser.pause(2000)
+//         const theLink = await $('p [href="/docs/sponsor"]')
+//         let isClickable = await theLink.isClickable()
+//         console.log("The link `becoming a sponsor` is clickable: " + isClickable)
+//         await theLink.click()
+//         await browser.pause(2000)
         
-    })
+//     })
 
-    xit("should get html for the link `becoming a sponsor`", async () => {
-        await browser.url(`https://webdriver.io`);
+//     xit("should get html for the link `becoming a sponsor`", async () => {
+//         await browser.url(`https://webdriver.io`);
 
-        const HTML = await $('p [href="/docs/sponsor"]').getHTML()
-        console.log("The `becoming a sponsor` link HTML is: " + HTML)
+//         const HTML = await $('p [href="/docs/sponsor"]').getHTML()
+//         console.log("The `becoming a sponsor` link HTML is: " + HTML)
 
-    })
+//     })
 
-    xit("should waitUntil ", async () => {
-        await browser.url(`https://webdriver.io/docs/sponsor/`);
+//     xit("should waitUntil ", async () => {
+//         await browser.url(`https://webdriver.io/docs/sponsor/`);
 
-        await browser.waitUntil(async () => {
-            return $('[id="how-to-sponsor"]').isDisplayed();
-        }, 5000, "The text `How to sponsor` is not displayed")
-    })
-})
+//         await browser.waitUntil(async () => {
+//             return $('[id="how-to-sponsor"]').isDisplayed();
+//         }, 5000, "The text `How to sponsor` is not displayed")
+//     })
+// })
---
+++ test/specs/semiankiv.art.js
@@ -0,0 +1,37 @@
+
+import { expect } from '@wdio/globals'
+
+
+describe("My website homework tests", () => {
+    xit("should be able to click on the Read More button", async () => {
+        await browser.url(`https://semiankiv.art/`)
+        await browser.pause(2000)
+
+
+        let readMoreButton = await $('//*[text()="Read more"]')
+        await readMoreButton.click()
+        await browser.pause(2000)
+
+    })
+
+    xit("The links section should contains the text Now Available", async () => {
+        await browser.url(`https://semiankiv.art/`)
+        await browser.pause(2000)
+
+
+        const theText = await $('[data-id="2f694763"]')
+        console.log("The link section contains the text: " + await theText.getText())
+        
+    })
+
+    it("The links section should contains the text Now Available", async () => {
+        await browser.url(`https://semiankiv.art/`)
+        await browser.pause(2000)
+
+
+        const buttonLearnMore = await $('//*[text()="Learn More"]')
+        console.log("The button Learn More is clickable: " + await buttonLearnMore.isClickable())
+        
+    })
+
+})
